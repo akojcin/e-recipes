8d1101ff6362f40f4dd2e32c8acc7ca63cebd81a
akojcin <kojcinovic.programming@gmail.com>
2022-02-25 12:18:07 +0100

    Project done

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const recipeRoutes = require('./routes/recipeRoutes');
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+
+// express app
+const app = express();
+
+// connect to mongodb
+
+mongoose.connect(process.env.DBURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(port))
+    .catch((err) => console.log(err));
+
+
+// register view engine
+app.set('view engine', 'ejs');
+
+
+// middleware static files
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
+
+// routes
+app.get('/', (req,res)=>{
+   res.redirect('/recipes'); 
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about', {title: 'About'});
+});
+
+
+//recipe routes
+app.use('/recipes', recipeRoutes);
+
+
+
+// 404 page
+app.use((req,res)=>{
+    res.status(404).render('404', {title: '404'});
+});
---
+++ controllers/recipeController.js
@@ -0,0 +1,62 @@
+// recipe_index, recipe_details, recipe_create_get, recipe_create_post, recipe_delete
+
+const Recipe = require('../models/recipe');
+
+
+
+const recipe_index = (req,res) => {
+    Recipe.find().sort({ createdAt: -1 })
+        .then((result)=>{
+            res.render('index', {title: 'All Recipes', recipes: result})
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+}
+
+const recipe_details = (req,res) => {
+    const id = req.params.id;
+    Recipe.findById(id)
+        .then((result)=>{
+            res.render('details', {recipe: result, title: 'Recipe Details'});
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+}
+ 
+const recipe_create_get = (req,res) => {
+    res.render('create', {title: 'Create a new recipe'});
+}
+
+const recipe_create_post = (req,res) => {
+    const recipe = new Recipe(req.body);
+
+ recipe.save()
+    .then((result)=>{
+        res.redirect('/recipes');
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+}
+
+const recipe_delete = (req, res) => {
+    const id = req.params.id;
+
+    Recipe.findByIdAndDelete(id)
+        .then((result)=>{
+            res.json({ redirect: '/recipes' })
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+}
+
+module.exports = {
+    recipe_index,
+    recipe_details,
+    recipe_create_get,
+    recipe_create_post,
+    recipe_delete
+}
